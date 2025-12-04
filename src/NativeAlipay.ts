7b26854ab54b3c3c07afb9acaa99a626ad707f1b
王云凯 <yunkai909@126.com>
2025-12-04 16:08:31 +0800

    新增registerApp 、getversion 方法

--- src/NativeAlipay.ts
+++ src/NativeAlipay.ts
@@ -2,10 +2,20 @@ import type { TurboModule } from "react-native/Libraries/TurboModule/RCTExport";
 import { TurboModuleRegistry } from "react-native";
 
 export interface Spec extends TurboModule {
-    alipay(orderInfo: string): Promise<string>;
-    setAlipayScheme(sScheme: string): void;
-    authInfo(urlStr: string, useLanding: boolean, sBundleName: string, sModuleName: string, sAbilityName: string,
-         callback: (para: unknown)=>void): Promise<void>;
+  registerApp(appID: string, universalLink: string);
+  getVersion(): Promise<string>;
+  alipay(orderInfo: string): Promise<string>;
+  setAlipayScheme(sScheme: string): void;
+  authInfo(
+    urlStr: string,
+    useLanding: boolean,
+    sBundleName: string,
+    sModuleName: string,
+    sAbilityName: string,
+    callback: (para: unknown) => void
+  ): Promise<void>;
 }
 
-export default TurboModuleRegistry.get<Spec>("AlipayNativeModule") as Spec | null;
+export default TurboModuleRegistry.get<Spec>(
+  "AlipayNativeModule"
+) as Spec | null;
